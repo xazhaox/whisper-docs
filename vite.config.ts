189f8f7aabf52e6d9bb67998482052323c87fdfa
xazhao <xazhao@126.com>
2024-09-03 17:49:33 +0800

    🎉 精简配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig, ConfigEnv, UserConfig } from "vite";
 
 // @see: https://vitejs.dev/config/
-export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
-	return {
-		server: {
-			host: "0.0.0.0",
-			port: 10837,
-			open: true,
-			cors: true,
-		},
-	};
+export default defineConfig({
+	server: {
+		host: "0.0.0.0",
+		open: true,
+	},
 });
